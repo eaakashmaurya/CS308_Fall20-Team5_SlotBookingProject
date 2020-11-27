15cead26992fc5e025a66169ca505eeeaa4fcc18
b18050 <abchandan11@gmail.com>
2020-11-27 15:56:48 +0530

    Refactored admin related code to admin folder

--- c4dfed.sql
+++ c4dfed.sql
@@ -43,4 +43,127 @@ INSERT INTO `equipment` VALUES (26,'DI water','Millipore',50,125,250,'per litre'
 INSERT INTO `equipment` VALUES (27,'Clean Lab Space (5x5 sq. foot)','---',2000,5000,10000,'per day');
 
 UNLOCK TABLES;
+--
+-- Table structure for table `tblcart`
+--
+
+CREATE TABLE `tblcart` (
+  `fld_cart_id` int(11) NOT NULL,
+  `fld_product_id` bigint(11) NOT NULL,
+  `fld_customer_id` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+--
+-- Table structure for table `tblcustomer`
+--
+
+CREATE TABLE `tblcustomer` (
+  `fld_cust_id` int(10) NOT NULL,
+  `fld_name` varchar(30) NOT NULL,
+  `fld_email` varchar(30) NOT NULL,
+  `fld_mobile` bigint(10) NOT NULL,
+  `password` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `tbadmin` (
+  `fld_id` int(10) NOT NULL,
+  `fld_username` varchar(30) NOT NULL,
+  `fld_password` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tblcustomer`
+--
+
+INSERT INTO `tblcustomer` (`fld_cust_id`, `fld_name`, `fld_email`, `fld_mobile`, `password`) VALUES
+(1, 'gajender', 'customer1@gmail.com', 7503515382, 'customer1'),
+(2, 'sanjay', 'customer2@gmail.com', 7503515386, 'customer2'),
+(3, 'saana', 'customer3@gmail.com', 7503515383, 'customer3');
+
+--
+-- Table structure for table `tblorder`
+--
+
+CREATE TABLE `tblorder` (
+  `fld_order_id` int(10) NOT NULL,
+  `fld_cart_id` bigint(10) NOT NULL,
+  `fldvendor_id` bigint(10) DEFAULT NULL,
+  `fld_food_id` bigint(10) DEFAULT NULL,
+  `fld_email_id` varchar(50) DEFAULT NULL,
+  `fld_payment` varchar(20) DEFAULT NULL,
+  `fldstatus` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tblorder`
+--
+
+INSERT INTO `tblorder` (`fld_order_id`, `fld_cart_id`, `fldvendor_id`, `fld_food_id`, `fld_email_id`, `fld_payment`, `fldstatus`) VALUES
+(1, 1, 21, 1, 'customer3@gmail.com', '50', 'Delivered'),
+(2, 2, 22, 3, 'customer3@gmail.com', '20', 'Out Of Stock');
+
+--
+-- Indexes for table `tbadmin`
+--
+ALTER TABLE `tbadmin`
+  ADD PRIMARY KEY (`fld_id`);
+
+--
+-- Indexes for table `tbfood`
+--
+ALTER TABLE `tbfood`
+  ADD PRIMARY KEY (`food_id`),
+  ADD KEY `fldvendor_id` (`fldvendor_id`);
+
+--
+-- Indexes for table `tblcart`
+--
+ALTER TABLE `tblcart`
+  ADD PRIMARY KEY (`fld_cart_id`);
+
+--
+-- Indexes for table `tblcustomer`
+--
+ALTER TABLE `tblcustomer`
+  ADD PRIMARY KEY (`fld_cust_id`);
+--
+-- Indexes for table `tblorder`
+--
+ALTER TABLE `tblorder`
+  ADD PRIMARY KEY (`fld_order_id`);
+
+
+--
+-- AUTO_INCREMENT for table `tbadmin`
+--
+ALTER TABLE `tbadmin`
+  MODIFY `fld_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tbfood`
+--
+ALTER TABLE `tbfood`
+  MODIFY `food_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `tblcart`
+--
+ALTER TABLE `tblcart`
+  MODIFY `fld_cart_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tblcustomer`
+--
+ALTER TABLE `tblcustomer`
+  MODIFY `fld_cust_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+--
+-- AUTO_INCREMENT for table `tblorder`
+--
+ALTER TABLE `tblorder`
+  MODIFY `fld_order_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
