e3e730c9cfa10be875a3722fc59c5bfd0aa3ee15
b18050 <abchandan11@gmail.com>
2020-11-29 01:30:57 +0530

    Contact us added with website for c4dfed

--- contactus.php
+++ contactus.php
@@ -47,26 +47,19 @@ session_start();
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="bookslots.php">Book Slots</a>
+          
         </div>
 
         <div class="collapse navbar-collapse " id="myNavbar">
           	<ul class="nav navbar-nav">
 				<li class="active" ><a href="index.php">Home</a></li>
 				<li><a href="aboutus.php">About</a></li>
-				<li><a href="contactus.php">Contact Us</a></li>
+				
 
           	</ul>
 
-            <ul class="nav navbar-nav navbar-right">
-				
-					<li> <a href="customersignup.php"> User Sign-up</a></li>
-					<li> <a href="adminsignup.php"> Admin Sign-up</a></li>
-             		<li> <a href="customerlogin.php"> User Login</a></li>
-              		<li> <a href="adminlogin.php"> Admin Login</a></li>
-             
-            </ul>
-
+         
+          
 
 		  
       </div>
